839c2729f88e175950ffcb7dd349d124801623b8
jhampton <jhampton@gitlab.com>
2020-02-14 16:02:46 -0500

    Adds files state

--- signed-urls-ui/src/App.js
+++ signed-urls-ui/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import FileUpload from './components/FileUpload';
+import { getBucketFiles } from './lib/cloud-storage';
 
 function App() {
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const files = await getBucketFiles();
+      setFiles(files);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <FileUpload files={[]} />
+        <FileUpload />
+
+        {files.map(f => {
+          return <span key={f.name}>{f.name}</span>;
+        })}
       </header>
     </div>
   );
