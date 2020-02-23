cd17b316263fb88e71d0b23f227662cb3d162d2e
jhampton <jhampton@gitlab.com>
2020-02-23 14:43:01 -0500

    Fixes test

--- cloud-storage-ui/src/App.test.js
+++ cloud-storage-ui/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders add files text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Add files by clicking or dragging into this area/i);
   expect(linkElement).toBeInTheDocument();
 });
